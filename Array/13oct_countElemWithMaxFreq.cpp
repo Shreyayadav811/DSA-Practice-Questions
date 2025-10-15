7d257d91633b14ac4168f4e7a54050e147e7b5be
Shreya Yadav <yadavshreya8989@gmail.com>
2025-10-16 00:01:32 +0530

    counting max frequency elements

---
+++ Array/13oct_countElemWithMaxFreq.cpp
@@ -0,0 +1,26 @@
+/* Problem: count elements with maximum frequency
+  Link : count elements with maximum frequency
+*/
+
+
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        unordered_map<int,int>freq;
+        for(int n: nums){
+            freq[n]++;
+        }
+        int maxFreq = 0;
+        for(auto &p : freq){
+            maxFreq = max(maxFreq, p.second);
+        }
+        int ans = 0;
+        for(auto &p : freq){
+            if(p.second == maxFreq){
+                ans += p.second; 
+            }
+        }
+
+        return ans;
+    }
+};
