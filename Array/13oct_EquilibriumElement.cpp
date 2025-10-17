0d01103fa7761185fd8cd925ab060c47a0233ad6
Shreya Yadav <yadavshreya8989@gmail.com>
2025-10-17 23:35:48 +0530

    find pivot index
    Implement solution to find pivot index in an array.

---
+++ Array/13oct_EquilibriumElement.cpp
@@ -0,0 +1,20 @@
+/*
+Problem: find pivot index
+Link: https://leetcode.com/problems/find-pivot-index/description/
+*/
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int totalSum = 0;
+        for(int num:nums) totalSum += num;
+
+        int leftSum = 0;
+        for(int i=0; i<nums.size(); i++){
+            int rightSum = totalSum-leftSum-nums[i];
+            if(leftSum == rightSum) return i;
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+};
