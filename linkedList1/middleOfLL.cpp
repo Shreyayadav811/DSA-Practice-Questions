ee957188874f66e1c0505d7359e077a5167f6687
Shreya Yadav <yadavshreya8989@gmail.com>
2025-10-30 14:25:19 +0530

    middle node of linked list
    Implemented a solution to find the middle of a linked list using the fast and slow pointer technique.

---
+++ linkedList1/middleOfLL.cpp
@@ -0,0 +1,25 @@
+/*
+Problem: find middle of a linked list
+
+Approach: use fast and slow pointer both starting from head, slow moves one step, fast moves 2 steps
+*/
+
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+        while(fast!=NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
