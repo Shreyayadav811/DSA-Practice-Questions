061c20e98bd66cee7502951ed5aec8c67edbf768
Shreya Yadav <yadavshreya8989@gmail.com>
2025-11-02 23:46:57 +0530

    linked list cycle detection
    Implement solution to detect cycle in a linked list.

---
+++ LinkedList1/linkedListCycle.cpp
@@ -0,0 +1,28 @@
+/*
+Problem: linked list cycle
+Link: https://leetcode.com/problems/linked-list-cycle/
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head) return false;
+        ListNode*slow = head, *fast = head;
+
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow==fast) return true;   //cycle exits
+        }
+        return false;   //cycle doesnt exists
+    }
+};
